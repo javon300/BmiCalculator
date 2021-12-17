f5ff0bf72d7915052d33eec9e1da78b4725611a7
matthew maxwell <matthewmaxwell300@yahoo.com>
2021-12-17 11:24:46 -0700

    navigation and first screen

---
+++ BmiCalculator/SubViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SubViewController.swift
+//  BmiCalculator
+//
+//  Created by javon maxwell on 2021-12-17.
+//
+
+import UIKit
+
+class SubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BmiCalculator/ViewController.swift
+++ BmiCalculator/ViewController.swift
@@ -7,11 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    //variables
+    var name: String?
+    var age: String?
+    var gender: String?
+    var weight = 0.0
+    var height = 0.0
+    var result = 0.0
+    
+    //UIcomponents connections
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var ageTF: UITextField!
+    @IBOutlet weak var genderTF: UITextField!
+    @IBOutlet weak var weightTF: UITextField!
+    @IBOutlet weak var heightTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var unitType: UISwitch!
+    
+    @IBAction func calculateBtn(_ sender: UIButton)
+    {
+        name = nameTF.text
+        age = ageTF.text
+        gender = genderTF.text
+        weight = NSString(string: weightTF.text!).doubleValue
+        height = NSString(string: heightTF.text!).doubleValue
+        if (unitType.isOn)
+        {
+            result = weight/height
+            resultLabel.text = formatResult(result: result)
+            
+        }else
+        {
+            result = (weight/height/height)*10000
+            resultLabel.text = formatResult(result: result)
+        }
+        
+    }
+    
+    func formatResult(result: Double) -> String{
+            return String(format: "%.6f",result)
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 

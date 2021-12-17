8757f17ade814c194aa5dccc41cdd023b00d84eb
Matthew Maxwell <mattmatthewmaxwell2014@outlook.com>
2021-12-16 19:29:27 -0500

    Initial Commit

---
+++ BmiCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BmiCalculator
+//
+//  Created by javon maxwell on 2021-12-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
